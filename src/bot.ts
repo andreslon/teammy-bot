2fadc7110ef683682c5aead284c54d8ce912f024
stivendiaz <stivendiazagudelo@gmail.com>
2019-07-08 12:09:46 -0500

    Ts lint configuration fix #1 (#2)
    * Configure TS-Lint
    
    * Fix linter warnings
    
    * Fix missing dependencies and rules

--- src/bot.ts
+++ src/bot.ts
@@ -4,24 +4,24 @@
 import { ActivityHandler } from 'botbuilder';
 
 export class MyBot extends ActivityHandler {
-    constructor() {
-        super();
+  constructor() {
+    super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        this.onMessage(async (context, next) => {
-            await context.sendActivity(`You said '${ context.activity.text }'`);
+    this.onMessage(async (context, next): Promise <void> => {
+      await context.sendActivity(`You said '${ context.activity.text }'`);
             // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
+      await next();
+    });
 
-        this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            for (const member of membersAdded) {
-                if (member.id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hello and welcome!');
-                }
-            }
+    this.onMembersAdded(async (context, next): Promise <void> => {
+      const membersAdded = context.activity.membersAdded;
+      for (const member of membersAdded) {
+        if (member.id !== context.activity.recipient.id) {
+          await context.sendActivity('Hello and welcome!');
+        }
+      }
             // By calling next() you ensure that the next BotHandler is run.
-            await next();
-        });
-    }
+      await next();
+    });
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -18,24 +18,24 @@ config({ path: ENV_FILE });
 // Create HTTP server.
 const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${server.name} listening to ${server.url}`);
-    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
-    console.log(`\nSee https://aka.ms/connect-to-bot for more information`);
+  console.log(`\n${server.name} listening to ${server.url}`);
+  console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+  console.log(`\nSee https://aka.ms/connect-to-bot for more information`);
 });
 
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about adapters.
 const adapter = new BotFrameworkAdapter({
-    appId: process.env.MicrosoftAppID,
-    appPassword: process.env.MicrosoftAppPassword,
+  appId: process.env.MicrosoftAppID,
+  appPassword: process.env.MicrosoftAppPassword,
 });
 
 // Catch-all for errors.
 adapter.onTurnError = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
-    console.error(`\n [onTurnError]: ${ error }`);
+  console.error(`\n [onTurnError]: ${ error }`);
     // Send a message to the user
-    await context.sendActivity(`Oops. Something went wrong!`);
+  await context.sendActivity(`Oops. Something went wrong!`);
 };
 
 // Create the main dialog.
@@ -43,8 +43,8 @@ const myBot = new MyBot();
 
 // Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
+  adapter.processActivity(req, res, async (context) => {
         // Route to main dialog.
-        await myBot.run(context);
-    });
+    await myBot.run(context);
+  });
 });
