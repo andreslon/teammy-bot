b0d0969101210524c25671132c597188218a3bd9
Juan David Henao Zapata <jhenaoz93@gmail.com>
2019-07-16 14:33:03 -0500

    Add Decrytp and Encript (#5)
    * Add Decrytp and Encript
    
    * Update encrypt.decrypt.js

---
+++ scripts/encrypt.decrypt.js
@@ -0,0 +1,24 @@
+const crypto = require('crypto');
+const  config  = require('config');
+const password = config.google.decrypt_password;
+const algorithm = config.encrypt_algorithm;
+
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+ 
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+
+module.exports = {
+    encrypt,
+    decrypt
+}
--- scripts/google.json.generator.js
+++ scripts/google.json.generator.js
@@ -1,11 +1,12 @@
 const  config  = require('config');
 const fs = require('fs');
+const { decrypt }  = require('./encrypt.decrypt');
 
 const obj = {
     type: config.get('google.type') ,
     project_id: config.get('google.project_id'),
     private_key_id: config.get('google.private_key_id'),
-    private_key: `-----BEGIN PRIVATE KEY-----\n${config.get('google.private_key')}\n-----END PRIVATE KEY-----\n`,
+    private_key: decrypt(config.get('google.private_key')),
     client_email: config.get('google.client_email'),
     client_id: config.get('google.client_id'),
     auth_uri: config.get('google.auth_uri'),
@@ -13,7 +14,6 @@ const obj = {
     auth_provider_x509_cert_url: config.get('google.auth_provider_x509_cert_url'),
     client_x509_cert_url: config.get('google.client_x509_cert_url')
 }
-obj.private_key = obj.private_key.replace(/\\n/g, "\n");
 
 var json = JSON.stringify(obj);
 
